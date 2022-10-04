d98757e8f3305d26c669507d87c1694761549222
kristy124 <112285593+kristy124@users.noreply.github.com>
2022-10-03 22:04:01 -0400

    Update caesarCipherFinal.py

--- caesarCipherFinal.py
+++ caesarCipherFinal.py
@@ -2,7 +2,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
