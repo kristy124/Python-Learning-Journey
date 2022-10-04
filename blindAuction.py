a313f9d399f190bca423e735fba924dfa9a91740
kristy124 <112285593+kristy124@users.noreply.github.com>
2022-10-04 12:04:45 -0400

    Update blindAuction.py

--- blindAuction.py
+++ blindAuction.py
@@ -3,16 +3,6 @@ from auctionArt import logo
 print(logo)
 print('Welcome to the secret auction program.')
 
-# Udemy Solution
-# def find_highest_bidder(bidding_record):
-#     highest_bid = 0
-#     highest_bidder = ''
-#     for name in bidding_record:
-#         if bidding_record[name] > highest_bid:
-#             highest_bid = bidding_record[name]
-#             highest_bidder = name
-#     print(f'The winner is {highest_bidder} with a bid of ${highest_bid}')
-
 bids = {}
 continue_auction = True
 while continue_auction:
@@ -22,7 +12,6 @@ while continue_auction:
     other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
     if other_bidders == 'no':
         continue_auction = False
-        find_highest_bidder(bids)
     # clear the screen
 
 #clear the screen
