466d689db154138c14f12453a698c843a50dc03e
kristy124 <112285593+kristy124@users.noreply.github.com>
2022-10-11 14:19:44 -0400

    Update higherLowerFinal.py

--- higherLowerFinal.py
+++ higherLowerFinal.py
@@ -1,7 +1,17 @@
 import random
+from os import system, name
 
-import higherLowerArt
+from higherLowerArt import logo, vs
 from higherLowerGameData import data
+
+
+def clear():
+    """Clears the terminal"""
+    if name == 'nt':
+        _ = system('cls')
+
+    else:
+        _ = system('clear')
 
 def random_account():
     """Returns a random account from data"""
@@ -21,19 +31,24 @@ def play():
     account_a = random_account()
     account_b = random_account()
     while account_a == account_b:
-        account_b = random_account()
+        account_b - random_account()
     current_score = 0
     playing_game = True
-    print(higherLowerArt.logo)
+    print(logo)
     while playing_game:
         account_a = account_b
         account_b = random_account()
+        if account_a == account_b:
+            account_b = random_account()
         print(f"Compare A: {print_data_info(account_a)}")
-        print(higherLowerArt.vs)
+        print(vs)
         print(f"Against B: {print_data_info(account_b)}")
         guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-        # clear screen
-        print(higherLowerArt.logo)
+        while guess != 'a' and guess != 'b':
+            print("Please enter a valid input.")
+            guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        clear()
+        print(logo)
         if account_a['follower_count'] > account_b['follower_count'] and guess == "a":
             current_score += 1
             print(f"You're right! Current score: {current_score}")
@@ -49,6 +64,12 @@ def play():
         else:
             print(f"Sorry, that's wrong. Final score: {current_score}")
             playing_game = False
-
+    play_again = input("Would you like to play again? Enter 'Y' or 'N'. ").lower()
+    while play_again != 'y' and play_again != 'n':
+        print("Please enter a valid input.")
+        play_again = input("Would you like to play again? Enter 'Y' or 'N'. ").lower()
+    if play_again == "y":
+        clear()
+        play()
 
 play()
